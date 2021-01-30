1007cf16188c6dfa0e469fccf4d88f1000992a44
GuilhermeGPGil <gg.gil@campus.fct.unl.pt>
2021-01-30 16:52:37 +0000

    Add movement

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // Create renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -10,6 +11,16 @@ scene.background = new THREE.Color( 0xeeeeee );
 // Create camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+window.addEventListener( 'resize', onWindowResize );
 
 const photo_geometry = new THREE.PlaneGeometry( 1, 1, 1 );
 const photo_material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
@@ -31,19 +42,89 @@ photo2.position.x += 2;
 photo2.position.y -= 1;
 photo2.position.z -= 1;
 
+let moveLeft = false;
+let moveRight = false;
+let moveForward = false;
+let moveBackward = false;
+
+var onKeyDown = function ( event ) {
+    switch ( event.keyCode ) {
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = true; break;
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+        case 32: // space
+            if ( canJump === true ) velocity.y += 350;
+            canJump = false;
+            break;
+    }
+};
+var onKeyUp = function ( event ) {
+    switch( event.keyCode ) {
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+    }
+};
+
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
+
+
+
+
 // Game logic
 const update = function () {
-
+    if (moveForward) {
+        camera.position.z -= 1/60;
+    }
+    if (moveBackward) {
+        camera.position.z += 1/60;
+    }
+    if (moveLeft) {
+        camera.position.x -= 1/60;
+    }
+    if (moveRight) {
+        camera.position.x += 1/60;
+    }
 };
+
+
+
 
 // Render logic
 const render = function () {
 
     photo.rotation.y += 0.01;
     photo2.rotation.y -= 0.02;
-
     renderer.render( scene, camera );
 };
+
+
 
 // Main loop game
 const game_loop = function () {
